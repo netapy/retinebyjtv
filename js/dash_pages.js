14ea74c9d18bfc23f358036896f110f7ed5e58f3
netapy <39877685+netapy@users.noreply.github.com>
2022-05-16 13:20:06 +0200

    newgraphs

--- js/dash_pages.js
+++ js/dash_pages.js
@@ -18,7 +18,7 @@ function changePage(destinationHTML, loadingFun, sondageNum = 0) {
 };
 
 //PROJECTS OVERVIEW
-let dashProj = '<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom"><div></div><h1 class="h2">Sondages</h1></div><div id="mesSondages" class="row p-3 w-100"><div class="col-12 col-sm-6 col-lg-4 mb-4"><div class="neuProjet d-flex align-items-center justify-content-center text-center" style="height: 200px;" onclick="creatorTabBtn.click();">Créer un<br>nouveau projet</div></div></div>';
+let dashProj = '<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom"><div></div><h1 class="h2">Sondages</h1></div><div id="mesSondages" class="row p-3 w-100" style="max-width:1100px;"><div class="col-12 col-sm-6 col-lg-4 mb-4"><div class="neuProjet d-flex align-items-center justify-content-center text-center" style="height: 200px;" onclick="creatorTabBtn.click();">Créer un<br>nouveau projet</div></div></div>';
 
 let templateUserProj = '<div class="col-12 col-sm-6 col-lg-4 mb-5"><div class="neuProjet d-flex align-items-center justify-content-stretch flex-column hvr-float" onclick="changePage(dashSond,loadDataList,##IDPROJ##)"><div class="threeDot hvr-bounce-in" onclick="shareBtn(\'##IDPROJ##\');window.event.stopPropagation();"><img class="w-100" src="/img/icons/share.svg"></div><h3 class="p-3 pr-5 w-100 h-75 m-0" style="background-color: ##PROJCOLOR##; border-radius:  10px  10px 0px 0px; color: white;">##NOMPROJ##</h3><div class="px-3 flex-fill d-flex justify-content-between align-items-center w-100"><div class="d-fk">##CREADATE##</div><div>##NBREP## réponses</div></div></div></div>';
 
@@ -32,9 +32,9 @@ const loadProjList = async () => {
 };
 
 //SONDAGE DISPLAY
-let dashSond = '<div id="headerBar" class="text-center w-100 py-2 mb-3 mt-5"></div><div id="mesResultats" class="row p-1"></div>'
+let dashSond = '<div id="headerBar" class="text-center w-100 py-2 mb-3 mt-5"></div><div id="mesResultats" class="row p-1 mx-auto" style="max-width:1100px"></div>'
 
-let templateDataList = '<div class="neuProjetSimple p-4 text-center" data-aos="fade-up" data-aos-delay="50"><div><h5>##TTRQUEST##</h5><div class="nbRepGraph">🧍 ##REPS## répondants</div><div class="canvContain"></div></div><button class="screenBtn" onclick="copyGraph(this.previousSibling)">📄 Copier le graphique</button></div>'
+let templateDataList = '<div class="neuProjetSimple p-4 text-center" data-aos="fade-up" data-aos-delay="100"><div><h5>##TTRQUEST##</h5><div class="nbRepGraph">🧍 ##REPS## répondants</div><div class="canvContain"></div></div><div class="d-flex align-itemps-center justify-content-between "><button class="screenBtn" onclick="copyGraph(this.parentElement.previousSibling)">📄 Copier le graphique</button></div></div>'
 
 const loadDataList = async (num) => {
     document.querySelector("#header-toggle").parentElement.insertAdjacentHTML('beforeEnd', '<i class="bx bx-arrow-back" id="header-toggle" onclick="changePage(dashProj, loadProjList);"></i>')
@@ -95,14 +95,14 @@ const loadDataList = async (num) => {
             elem.innerHTML = templateDataList.replace("##TTRQUEST##", ii.toString() + ". " + projectQuestions[ii]["q"]).replace("##REPQ##", JSON.stringify(sondageData["data"][ii]["d"])).replace("##REPS##", sondageData["data"][ii]['nbrep']);
             if (['mc', '1c'].includes(sondageData["data"][ii]["t"])) {
                 let dataMc = sondageData["data"][ii]["d"];
-
                 elem.querySelector('.canvContain').insertAdjacentHTML('afterbegin', '<canvas></canvas>');
                 new Chart(elem.querySelector('canvas'), {
-                    type: 'pie',
+                    plugins: [ChartDataLabels],
+                    type: 'doughnut',
                     data: {
-                        labels: dataMc,
+                        labels: Object.keys(dataMc),
                         datasets: [{
-                            data: dataMc,
+                            data: Object.values(dataMc),
                             backgroundColor: chartColors,
                         }]
                     },
@@ -135,16 +135,25 @@ const loadDataList = async (num) => {
                         labels: Object.keys(sondageData["data"][ii]["d"]),
                         datasets: [{
                             label: "",
-                            color:'#6219D8',
+                            color: '#6219D8',
                             //TODO ajouter une règle pour varier l'échelle dynamiquement en fonction de la somme des valeurs
                             data: Object.values(sondageData["data"][ii]["d"]).map((d) => 10 + d * 5),
                         }, ],
                     },
                     options: {
-                        plugins:{
-                            legend:{
-                                display:false
-                            }
+                        plugins: {
+                            legend: {
+                                display: false
+                            },
+                            tooltips: {
+                                mode: 'label',
+                                callbacks: {
+                                    label: function (tooltipItem, data) {
+                                        var indice = tooltipItem.index;
+                                        return data.labels[indice] + ': ' + data.datasets[0].data[indice] + '';
+                                    }
+                                }
+                            },
                         }
                     },
                 };
@@ -152,30 +161,55 @@ const loadDataList = async (num) => {
                 new ChartWordCloud.WordCloudChart(ctx, config);
 
             } else if (['5s'].includes(sondageData["data"][ii]["t"])) {
-                let uniqueLabels = ["⭐⭐⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐", "⭐⭐", "⭐"];
+                let uniqueLabels = ["⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"];
                 let dataMc = sondageData["data"][ii]["d"]['detail'];
 
-                elem.querySelector('.canvContain').insertAdjacentHTML('afterbegin', '<canvas></canvas>');
-                new Chart(elem.querySelector('canvas'), {
-                    type: 'line',
-                    data: {
-                        datasets: [{
-                            data: dataMc,
-                            backgroundColor: "#6219D8",
-                        }]
-                    },
-                    options: {
-                        indexAxis: 'y',
-                        scales: scaleParam,
-                        responsive: true,
-                        maintainAspectRatio: false,
-                        plugins: {
-                            legend: {
-                                display: false
+                starHtml = '<div class="starContain py-3"><div class="mb-3">Note moyenne de ##STARS##</div><div class="stars"> <svg viewBox="0 0 576 512" width="50" title="star"> <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/> </svg><svg viewBox="0 0 576 512" width="50" title="star"> <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/> </svg><svg viewBox="0 0 576 512" width="50" title="star"> <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/> </svg><svg viewBox="0 0 576 512" width="50" title="star"> <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/> </svg><svg viewBox="0 0 576 512" width="50" title="star"> <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/> </svg> <div class="cover" style="width: ##PERCENT##%;"></div></div></div>'.replace("##PERCENT##", 100 - (sondageData["data"][ii]["d"]["main"] * 100) / 5).replace("##STARS##", sondageData["data"][ii]["d"]["main"]);
+
+                const toggleCanv = (btn) => {
+                    if (btn.target.dataset.state == "simple") {
+                        elem.querySelector('.canvContain').innerHTML = starHtml;
+                        btn.target.dataset.state = "detail";
+                    } else {
+                        console.log(dataMc);
+                        elem.querySelector('.canvContain').innerHTML = "<canvas></canvas>";
+                        new Chart(elem.querySelector("canvas"), {
+                            type: 'bar',
+                            data: {
+                                labels: uniqueLabels,
+                                datasets: [{
+                                    data: Object.values(dataMc),
+                                    backgroundColor: "#6219D8",
+                                }]
                             },
-                        }
+                            options: {
+                                indexAxis: 'y',
+                                scales: scaleParam,
+                                responsive: true,
+                                maintainAspectRatio: false,
+                                plugins: {
+                                    legend: {
+                                        display: false,
+                                        reversed: true
+                                    },
+                                }
+                            }
+                        });
+                        btn.target.dataset.state = "simple";
                     }
-                });
+                }
+
+                let btnDetail = document.createElement("button");
+                btnDetail.innerHTML = "➕ Détails";
+                btnDetail.className = "screenBtn";
+                btnDetail.dataset.state = "yes";
+
+                btnDetail.addEventListener("click", toggleCanv, false);
+
+                elem.querySelector('.canvContain').innerHTML = starHtml;
+                elem.querySelector('.screenBtn').insertAdjacentElement('afterend', btnDetail);
+
+
             } else if (['num'].includes(sondageData["data"][ii]["t"])) {
                 if (1 == 1) {
                     console.log("ef");
